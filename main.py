7211f4c64626221e63a41e507cb67e79692a8ea5
ahmad <anurli264@gmail.com>
2021-05-07 20:50:29 +0700

    add note

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# @app is decoration of operation
+# get is operation of path
+# ('/') is path of opration
 @app.get('/')
+# this is path operation function
 def index():
     return {'data':{'name': 'nurli'}}
 

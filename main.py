e71792cc4030514c6cc7c2ab031af9ce8e2da1aa
ahmad <anurli264@gmail.com>
2021-05-08 12:23:00 +0700

    path parameter 30:46 API Docs - Swagger/redocs 41:39 Query parameters 45:56 Request Body 55:58

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 from fastapi import FastAPI
-
+from typing import Optional
+# use pydantic to creta BaseModel
+from pydantic import BaseModel
 app = FastAPI()
 
 # @app is decoration of operation
 # get is operation of path
 # ('/') is path of opration
-@app.get('/')
+# Query parameters
+@app.get('/blog')
 # this is path operation function
-def index():
-    return {'data':{'name': 'nurli'}}
+# accepted parameter limit
+# initial the parameters as default value when you access just a /blog path
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # check data type with return published
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
+
+# fast api read the file line by line
+# this same like '/blog/{id}'
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blog'}
 
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+# use parantasies calibrases{} to fetch
+@app.get('/blog/{id}')
+# accepted variable in function with str type default
+# define id as int example: id: int
+def show(id: int):
+    # fetch blog with id = id
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data': {'1','2'}}
+
+
+# create some class model for try post Request
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+# accept parameters from BaseModel blog
+def create_blog(blog: Blog):
+    # access the title to get value of request body blogs
+    return {'data': f"Blog is created with title as {blog.title}"}

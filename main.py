bc42eeb065e6020d4f9301873ce6244631ccf2b9
ahmad <anurli264@gmail.com>
2021-05-16 14:15:57 +0700

    Request Body 55:58 How to Debug 1:03:58 Pydantic Schema 1:10.46

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 from typing import Optional
 # use pydantic to creta BaseModel
 from pydantic import BaseModel
+import uvicorn
 app = FastAPI()
 
 # @app is decoration of operation
 # get is operation of path
 # ('/') is path of opration
 # Query parameters
+
+
 @app.get('/blog')
 # this is path operation function
 # accepted parameter limit
@@ -15,12 +18,14 @@ app = FastAPI()
 def index(limit=10, published: bool = True, sort: Optional[str] = None):
     # check data type with return published
     if published:
-        return {'data':f'{limit} published blogs from the db'}
+        return {'data': f'{limit} published blogs from the db'}
     else:
-        return {'data':f'{limit} blogs from the db'}
+        return {'data': f'{limit} blogs from the db'}
 
 # fast api read the file line by line
 # this same like '/blog/{id}'
+
+
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data': 'all unpublished blog'}
@@ -32,13 +37,13 @@ def unpublished():
 # define id as int example: id: int
 def show(id: int):
     # fetch blog with id = id
-    return {'data':id}
+    return {'data': id}
 
 
 @app.get('/blog/{id}/comments')
 def comments(id):
     # fetch comments of blog with id = id
-    return {'data': {'1','2'}}
+    return {'data': {'1', '2'}}
 
 
 # create some class model for try post Request
@@ -53,3 +58,7 @@ class Blog(BaseModel):
 def create_blog(blog: Blog):
     # access the title to get value of request body blogs
     return {'data': f"Blog is created with title as {blog.title}"}
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
